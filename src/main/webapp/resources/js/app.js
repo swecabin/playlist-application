000f1f6f1f57529a3c28169933faf3266c657acb
Swetha Sampath <swetha@Swethas-MacBook-Air.local>
2020-11-14 20:08:44 +0530

    HTML wiring for CUD endpoints

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
     	 stompClient.connect({}, function (frame) {
     	        stompClient.subscribe('/topic/playlists', function (message) {
-    	        	generateTable(message);
+    	        	generateTable(JSON.parse(message.body).text);
     	        	console.log(message);
     	        });
     	    });
@@ -27,32 +27,23 @@ $(document).ready(function(){
 });
 
 function getPlaylists() {
-  // Return a new promise.
   return new Promise(function(resolve, reject) {
-    // Do the usual XHR stuff
     var req = new XMLHttpRequest();
     req.open('GET', 'http://localhost:8080/v1/playlists');
 
     req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
       if (req.status == 200) {
-        // Resolve the promise with the response text
         resolve(req.response);
       }
       else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
         reject(Error(req.statusText));
       }
     };
 
-    // Handle network errors
     req.onerror = function() {
       reject(Error("Network Error"));
     };
 
-    // Make the request
     req.send();
   });
 }
@@ -62,17 +53,14 @@ function generateTable(playlists) {
 
         var jsonPlaylist = JSON.parse(playlists);
 
-        //Create a HTML Table element.
         var table = document.createElement("TABLE");
         table.border = "1";
 
-        //Get the count of columns.
         var columnCount = 3;
 
         var headers = new Array();
         headers.push(["Name", "Songs", ""]);
 
-        //Add the header row.
         var row = table.insertRow(-1);
         for (var i = 0; i < columnCount; i++) {
             var headerCell = document.createElement("TH");
@@ -80,7 +68,6 @@ function generateTable(playlists) {
             row.appendChild(headerCell);
         }
 
-        //Add the data rows.
         for (var i = 0; i < jsonPlaylist.length; i++) {
             row = table.insertRow(-1);
 
@@ -101,9 +88,15 @@ function generateTable(playlists) {
                   singers.innerHTML = jsonPlaylist[i].songs[j].singers;
                   songRow.appendChild(singers);
 
+                  //Remove song from playlist
                   var deleteButton = document.createElement("BUTTON");
                   deleteButton.setAttribute('id', jsonPlaylist[i].songs[j].songID);
+                  deleteButton.setAttribute('playlistId', jsonPlaylist[i].playlistId);
                   deleteButton.textContent = 'Remove song';
+                  deleteButton.addEventListener('click', function() {
+                    removeSongFromPlaylist(deleteButton.getAttribute('playlistId'), deleteButton.getAttribute('id'));
+                  });
+
                   songRow.appendChild(deleteButton);
 
                   songTable.appendChild(songRow);
@@ -117,6 +110,9 @@ function generateTable(playlists) {
                 var addSongButton = document.createElement("BUTTON");
                 addSongButton.setAttribute('id', jsonPlaylist[i].playlistId);
                 addSongButton.textContent = 'Add song';
+                addSongButton.addEventListener('click', function() {
+                  addSongToPlaylist(addSongButton.getAttribute('id'));
+                });
                 c3.appendChild(addSongButton);
 
 
@@ -124,7 +120,9 @@ function generateTable(playlists) {
                 var deletePlaylistButton = document.createElement("BUTTON");
                 deletePlaylistButton.setAttribute('id', jsonPlaylist[i].playlistId);
                 deletePlaylistButton.textContent = 'Delete playlist';
-                deletePlaylistButton.onclick = deletePlaylist(jsonPlaylist[i].playlistId);
+                deletePlaylistButton.addEventListener('click', function() {
+                  deletePlaylist(deletePlaylistButton.getAttribute('id'));
+                });
                 c3.appendChild(deletePlaylistButton);
 
 
@@ -132,11 +130,15 @@ function generateTable(playlists) {
         }
 
         table.appendChild(row.insertCell(-1));
+
         //Add new playlist button in last row
-        var addNewPlaylistButton = document.createElement("BUTTON");
-        addNewPlaylistButton.setAttribute('id', 'add-playlist');
-        addNewPlaylistButton.textContent = 'Add new playlist';
-        table.appendChild(addNewPlaylistButton);
+        var createPlaylistButton = document.createElement("BUTTON");
+        createPlaylistButton.setAttribute('id', 'add-playlist');
+        createPlaylistButton.textContent = 'Add new playlist';
+        createPlaylistButton.addEventListener('click', function() {
+            createPlaylist();
+        });
+        table.appendChild(createPlaylistButton);
 
 
         var dvTable = document.getElementById("playlist-table");
@@ -147,34 +149,108 @@ function generateTable(playlists) {
 
 function deletePlaylist(playlistID) {
 
-  // Return a new promise.
   return new Promise(function(resolve, reject) {
 
-    //playlistID = document.getElementById()
-    // Do the usual XHR stuff
     var req = new XMLHttpRequest();
     req.open('DELETE', 'http://localhost:8080/v1/playlists/'+playlistID);
 
     req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
       if (req.status == 204) {
-        // Resolve the promise with the response text
         resolve(req.response);
       }
       else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
         reject(Error(req.statusText));
       }
     };
 
-    // Handle network errors
     req.onerror = function() {
       reject(Error("Network Error"));
     };
 
-    // Make the request
     req.send();
+  });
+}
+
+function removeSongFromPlaylist(playlistID, songID) {
+    return new Promise(function(resolve, reject) {
+
+    var req = new XMLHttpRequest();
+    req.open('DELETE', 'http://localhost:8080/v1/playlists/'+playlistID+'/songs/'+songID);
+
+    req.onload = function() {
+      if (req.status == 204) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    req.send();
+  });
+}
+
+function addSongToPlaylist(playlistID) {
+return new Promise(function(resolve, reject) {
+
+    var req = new XMLHttpRequest();
+
+    // create a JSON object
+    var payload = {
+        "songName": Math.random().toString(36).substr(2, 5),
+        "singers": Math.random().toString(36).substr(2, 10),
+        "releaseYear": 2019
+    };
+    req.open('POST', 'http://localhost:8080/v1/playlists/'+playlistID+'/add');
+    req.setRequestHeader('Content-Type', 'application/json');
+
+    req.onload = function() {
+      if (req.status == 201) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    req.send(JSON.stringify(payload));
+  });
+}
+
+
+function createPlaylist() {
+return new Promise(function(resolve, reject) {
+
+    var req = new XMLHttpRequest();
+
+    // create a JSON object
+    var payload = {
+        "playlistName": Math.random().toString(36).substr(2, 5)
+    };
+    req.open('POST', 'http://localhost:8080/v1/playlists');
+    req.setRequestHeader('Content-Type', 'application/json');
+
+    req.onload = function() {
+      if (req.status == 201) {
+        resolve(req.response);
+      }
+      else {
+        reject(Error(req.statusText));
+      }
+    };
+
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    req.send(JSON.stringify(payload));
   });
 }
